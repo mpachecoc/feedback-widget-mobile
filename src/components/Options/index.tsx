d1130be3b3759ecd9c2240b997b3bb64dad6abd0
Mauricio Pacheco <mauriciopachecoc@gmail.com>
2022-05-08 22:14:16 -0400

    Initial functionality, widget UI

---
+++ src/components/Options/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Copyright } from '../Copyright';
+import { Option } from '../Option';
+
+import { styles } from './styles';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export function Options() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Leave us your feedback</Text>
+
+      <View style={styles.options}>
+        { Object.entries(feedbackTypes).map(([key, value]) => (
+          <Option 
+            key={key} 
+            title={value.title} 
+            image={value.image} 
+          />
+        )) }
+      </View>
+
+      <Copyright />
+    </View>
+  );
+}
